040a958926c15681675c17b3fc6b26d0a6b803f5
Derek Jensen <jensendp@kreatived.com>
2016-03-31 11:49:29 -0500

    Initial Commit

---
+++ Tuts8Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tuts8Ball
+//
+//  Created by Derek Jensen on 3/31/16.
+//  Copyright © 2016 Kreative Dimensions, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
